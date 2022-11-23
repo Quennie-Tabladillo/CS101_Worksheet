e7197568c39671884a7c3c699b0e892d086e69ac
Quennie Tabladillo <quennie.tabladillo@students.isatu.edu.ph>
2022-11-23 05:22:57 +0000

    Worksheet 4

---
+++ Worksheet 4/RWorksheet_Tabladillo#4.R
@@ -0,0 +1,71 @@
+#Worksheet 4
+#Tabladillo,Quennie R.
+
+#1. The table shows the data about shoe size and height.Create a data frame.
+Customers <- data.frame(
+  Shoesize = c(6.5,9.0,8.5,8.5,10.5,7.0,9.5,9.0,13.0,
+              7.5,10.5,8.5,12.0,10.5,
+              13.0,11.5,8.5,5.0,10.0,
+             6.5,7.5,8.5,10.5,8.5,10.5,11.0,9.0,13.0),
+  Height = c(66.0,68.0,64.5,65.0,70.0,
+            64.0,70.0,71.0,72.0,64.0,
+            74.5,67.0,71.0,71.0,77.0,72.0,
+            59.0,62.0,72.0,66.0,64.0,67.0,73.0,
+            69.0,72.0,70.0,69.0,70),
+  Gender = c("F","F","F","F","M","F","M","F","M",
+            "M","M","F","M","M","M","M","F","F",
+            "M","F","M","M","M","F","M","M","M","M")
+)
+Customers
+
+#a.Describe the data.
+#The data is about the customers shoe size, height and gender.
+
+#b.Find the mean of shoe size and height of the respondents.
+#Copy the codes and results
+summary(Customers)
+#Shoesize:    Mean   : 9.411
+#Height:      Mean   :68.57
+
+#c. Is there a relationship between shoe size and height? Why?
+#Yes,because taller women tend to have larger feet since they need a larger
+#base for balance while shorter women tend to have smaller feet since 
+#they require a smaller base.
+
+#2.Construct character vector months to a factor with factor() and assign
+#the result to factor_months_vector. Print out factor_months_vector and 
+#assert that R prints out the factor levels below the actual values.
+Months <- c("March","April","January","November","January",
+            "September","October","September","November","August",
+            "January","November","November","February","May","August",
+            "July","December","August","August","September","November","February","April")
+
+factor_Months <- factor(Months)
+factor_Months
+
+#3.Then check the summary() of the months_vector and factor_months_vector. |
+#Interpret the results of both vectors. Are they both equally
+#useful in this case?
+summary(Months)
+summary(factor_Months)
+
+
+#4.Create a vector and factor for the table below.
+factor_data <- c(1,4,3)
+new_order_data <- factor(factor_data,levels = c("East","West","North"))
+print(new_order_data)
+
+#5. Enter the data below in Excel with file name = import_march.csv
+
+#a.Import the excel file into the environment pane using read.table() function
+getwd()
+readdata <- read.table("import_march.csv", header = TRUE, sep = ",")
+readdata
+
+#b. View thee data set.Write the code and its result.
+read.csv("import_march.csv")
+
+
+
+
+
